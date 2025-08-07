180b63dd7873e1dbeea929631cd7d9988a98c38f
liz <liz@Ubuntu-22.04.myguest.virtualbox.org>
2025-08-07 16:19:40 +0100

    Upload of minitask1 skeleton package

---
+++ minitask_1/minitask_1/minitask1.py
@@ -0,0 +1,69 @@
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+from nav_msgs.msg import Odometry
+
+class Minitask1(Node):
+
+    def __init__(self):
+        super().__init__('minitask1')
+        #create the publisher
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        #create the subscriber
+        self.subscription = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    #publish a message every 0.5 seconds
+    def timer_callback(self):
+        #create new message of type Twist
+        msg = Twist()
+        #create linear component
+        l = Vector3()
+        l.x = 1.0
+        l.y = 0.0
+        l.z = 0.0
+        #create angular component
+        a = Vector3()
+        a.x = 0.0
+        a.y = 0.0
+        a.z = 0.0
+
+        #set message linear and angular
+        msg.linear = l
+        msg.angular = a
+        #publish message
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing Twist...')
+
+    def odom_callback(self, msg):
+        location = msg.pose.pose.position
+        self.get_logger().info('Robot is at: "%s"' % location)
+
+
+    
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    mt = Minitask1()
+
+    rclpy.spin(mt)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    mt.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
